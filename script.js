fe6a35f0877a478ec1f55f9de08edb4b8e61e5dc
Rickard Almgren <almgren.rickard96@gmail.com>
2024-12-12 17:42:05 +0100

    added api styling

--- script.js
+++ script.js
@@ -2,6 +2,12 @@
 const button = document.getElementById('click');
 
 
-button.addEventListener('click', function() {
-  alert('Clicked!');
+button.addEventListener('click', async function() {
+  const response = await fetch ('https://meowfacts.herokuapp.com/');
+  const facts = await response.json()
+  document.getElementById('catFact').innerText = facts.data[0]
+  // console.log(facts.data[0])
 });
+
+
+
